f2fcaffc3d188e4dd2795edae9bdd2b7b0edc50c
xiuqi <summerlinsg@gmail.com>
2025-11-07 09:19:10 +0800

    Initial Commit

---
+++ challenge 3 app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  challenge 3 app
+//
+//  Created by Xiuqi Lin on 5/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ challenge 3 app/challenge_3_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_3_appApp.swift
+//  challenge 3 app
+//
+//  Created by Xiuqi Lin on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct challenge_3_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
