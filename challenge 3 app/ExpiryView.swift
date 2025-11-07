2be33f052e6628a6f169f900d0df18f529214ef3
ashley <leng.ashley12@gmail.com>
2025-11-07 10:06:21 +0800

    deleted TabView, made ExpiryView

---
+++ challenge 3 app/ExpiryView.swift
@@ -0,0 +1,24 @@
+//
+//  ExpiryView.swift
+//  challenge 3 app
+//
+//  Created by Ashley Leng on 7/11/25.
+//
+
+import SwiftUI
+
+struct ExpiryView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ExpiryView()
+}
