2dca7bf9011aa40214dca8717e55a5554d677fda
xiuqi <summerlinsg@gmail.com>
2025-11-19 17:02:56 +0800

    //

---
+++ challenge 3 app/Foundation/ConfirmationView.swift
@@ -0,0 +1,41 @@
+//
+//  ConfirmationView.swift
+//  challenge 3 app
+//
+//  Created by Ella Teo on 15/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ConfirmationView: View {
+    @Binding var foodItems: [FoodItem]
+    @Environment(\.modelContext) var modelContext
+    @Binding var navigate: Bool
+    var body: some View {
+        List{
+            ForEach($foodItems){ item in
+                NavigationLink(destination: EditDetailsView(item: item)){
+                    Text(item.nameOfFood.wrappedValue)
+                }
+        
+            }
+        }
+        .toolbar{
+            ToolbarItem(placement: .topBarTrailing){
+                Button{
+                    for food in foodItems{
+                        modelContext.insert(food)
+                    }
+                    navigate = false
+                    
+                }label: {
+                    Text("Save")
+                }
+                
+            }
+        }
+    }
+}
+
+
